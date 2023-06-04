742abf4fcfc5920b2478fcf66724864fbf724bb5
Seong Kim <runmath@gmail.com>
2023-06-03 22:50:41 -0600

    test done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(true);
+  function handleClick(){
+    setIsInCart((isInCart) => !isInCart)
+  }
+  const appClass = isInCart ? "" : "in-cart"
+  
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{isInCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
